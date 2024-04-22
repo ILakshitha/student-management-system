4f95ff796a72a6d0e2e4bfa755ed9231f421061e
ILakshitha <ilakshitha7921@gmail.com>
2024-04-22 23:31:50 +0530

    create nav bar and add function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 import CounterClass from './component/counterClass';
 import CounterFunction from './component/CounterFunction';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React </h1>
       <CounterClass/>
-      <CounterFunction/>                      
+      <CounterFunction/> 
+      <Headers/>
+
     </div>
   );
 }
